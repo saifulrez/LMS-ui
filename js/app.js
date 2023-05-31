041ff7bf2e2ac7cbbc8ff18107b601428c697190
saifulrez <89093211+saifulrez@users.noreply.github.com>
2023-05-31 11:01:08 +0800

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const passwordViewToggler = document.querySelector('.eye-toggler');
+const passwordInput = document.querySelector('#password');
+const closeEyeIcon = 'fa-eye-slash';
+const openEyeIcon = 'fa-eye';
+let isOpen = true;
+
+const togglePasswordView = (e) => {
+   isOpen = !isOpen;
+   if(isOpen === false){
+       passwordInput.type = 'text';
+       e.target.firstElementChild.className =  `fa ${openEyeIcon}`;
+   }else{
+       passwordInput.type = 'password';
+       e.target.firstElementChild.className =  `fa ${closeEyeIcon}`;
+   }
+}
+
+passwordViewToggler.addEventListener('click', togglePasswordView);
